66fd8aeeb46462fcf3d82978c9bf60bc3c13843f
Edison <edyasha00@gmail.com>
2022-11-13 22:37:14 +0200

    creating a header

--- index.php
+++ index.php
@@ -5,10 +5,75 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="css/style.css" rel="stylesheet">
+  <link rel="shortcut icon" href="img/icon.svg" type="image/svg" />
   <title>Discounts On Services</title>
 </head>
 <body>
-  <h3>Hello!</h3>
+  <div class="wrapper">
+    <header class="header">
+      <div class="header__container bounding-box">
+        <div class="header__logo logo">
+          <img class="logo__img" src="img/logo.png" alt="DiscountsOnServices">
+          <a class="logo__link" href="">DiscountsOnServices</a>
+        </div>
+        <nav class="header__menu menu">
+          <ul class="menu__list">
+            <li class="menu__item">
+              <a href="" class="menu__link">Subscriptions</a>
+              <ul class="menu__list sub-menu">
+                <li class="sub-menu__item">
+                  <a href="" class="sub-menu__link">Netflix</a>
+                </li>
+                <li class="sub-menu__item">
+                  <a href="" class="sub-menu__link">YouTube Premium</a>
+                </li>
+                <li class="sub-menu__item">
+                  <a href="" class="sub-menu__link">Spotify</a>
+                </li>
+              </ul>
+            </li>
+            <li class="menu__item">
+              <a href="" class="menu__link">FAQ</a>
+            </li>
+            <li class="menu__item">
+              <a href="" class="menu__link">Support</a>
+            </li>
+            <li class="menu__item">
+              <a href="" class="menu__link">About</a>
+            </li>
+          </ul>
+        </nav>
+        <div class="header__login login-box">
+          <nav class="login-box__networks networks">
+            <ul class="networks__list">
+              <li class="networks__item">
+                <a href="" class="networks__link"><img src="img/header-whatsapp.png" alt=""></a>
+              </li>
+              <li class="networks__item">
+                <a href="" class="networks__link"><img src="img/header-telegram.png" alt=""></a>
+              </li>
+            </ul>
+          </nav>
+          <nav class="login-box__lang lang">
+            <ul class="lang__list">EN
+              <li class="lang__item">
+                <a href="" class="lang__link">English</a>
+              </li>
+              <li class="lang__item">
+                <a href="" class="lang__link">Ukraine</a>
+              </li>
+              <li class="lang__item">
+                <a href="" class="lang__link">Russian</a>
+              </li>
+            </ul>
+          </nav>
+          <button class="login-box__login" type="button">Log in</button>
+        </div>
+      </div>
+    </header>
+    <main class="main"></main>
+    <footer class="footer"></footer>
+  </div>
   <script src="js/script.js"></script>
 </body>
 </html>
